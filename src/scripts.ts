a747c0ee6b6b2c91a0b25e57c2f8cdaa1b67cf28
Frédéric Camblor <frederic.camblor@4sh.fr>
2025-05-20 16:59:54 +0200

    provided solution into scripts.ts following tp1.1 p2 guidelines

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,1 +1,31 @@
 console.log(`Hello world !`)
+
+const aBool: boolean = true,
+  aString: string = "Hello",
+  aNum: number = 42;
+
+interface IPerson {
+  firstName: string,
+  lastName: string,
+}
+
+type TPerson = {
+  firstName: string,
+  lastName: string
+}
+
+class Person {
+  #age: number;
+  constructor(private firstName: string, private lastName: string, age: number) {
+    this.#age = age;
+  }
+}
+
+let person: Person = new Person("Anders", "Hejlsberg", 63);
+console.log(person);
+
+// Un commentaire qui devrait disparaître
+type TotalSizeOf = (str1: string, str2: string) => number;
+const totalSizeOf: TotalSizeOf = (str1, str2) => str1.length + str2.length;
+
+console.log(totalSizeOf("Hello", "world"));
