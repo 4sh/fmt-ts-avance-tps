32848ec923f9cdf378d096d102513a503eea0b23
Frédéric Camblor <frederic.camblor@4sh.fr>
2025-05-20 17:02:55 +0200

    preparing tp2.1 p1

---
+++ src/scripts.js
@@ -0,0 +1,42 @@
+
+async function loadPokemons() {
+  // Context starts from index.html (from where current script is imported)
+  const TP_ROOT_PATH = `./`
+  return await fetch(`${TP_ROOT_PATH}data/pokemons.json`).then(resp => resp.json())
+}
+
+function showPokemon(predicate) {
+  const pokemon = POKEMONS.find(predicate);
+  if(!pokemon) {
+    console.log(`No pokemon found matching predicate ${predicate.toString()}`)
+    return;
+  }
+
+  let text = `[${pokemon.id}] ${pokemon.name}
+    ${pokemon.stats.map(st => `${st.stat.name}:${st.base_stat}`).join(", ")}
+    Types: ${pokemon.types.map(t => t.type.name).join(", ")}
+    Abilities: ${pokemon.abilities.map(ab => ab.ability.name).join(", ")}
+  `
+  document.querySelector("#result").innerHTML = text
+}
+
+function findPokemonById() {
+  const id = document.querySelector("input")?.value;
+  showPokemon(pokemon => pokemon.id === id);
+}
+function findPokemonByName() {
+  const name = document.querySelector("input")?.value;
+  showPokemon(pokemon => pokemon.name.toLowerCase() === name?.toLowerCase());
+}
+
+let POKEMONS = [];
+async function main() {
+  POKEMONS = await loadPokemons();
+  console.log(`All ${POKEMONS.length} pokemons loaded successfully !`)
+
+  document.querySelector("#showPokemonById").addEventListener('click', findPokemonById);
+  document.querySelector("#showPokemonByName").addEventListener('click', findPokemonByName);
+  console.log("Button events initiated !")
+}
+
+main();
